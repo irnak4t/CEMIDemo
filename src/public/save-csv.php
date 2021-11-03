8bf79a6d1d92ad124f337c66778b5267cded7c79
irnak4t <ima.tak0610@gmail.com>
2021-11-04 00:45:56 +0900

    Fix wrong content type

--- src/public/save-csv.php
+++ src/public/save-csv.php
@@ -6,7 +6,7 @@ $sql = 'SELECT * FROM test_tbl';
 $sth = $pdo->query($sql);
 $data = $sth->fetchAll(PDO::FETCH_ASSOC);
 
-header('Content-Type: application/csv; charset=UTF-8');
+header('Content-Type: text/csv; charset=UTF-8');
 header('Content-Disposition: attachment; filename=cemi.csv');
 header('Content-Transfer-Encoding: binary');
 
