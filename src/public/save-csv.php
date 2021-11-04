7d064930e29d897f9bd0171b8cbb96a1bd4c7c44
irnak4t <ima.tak0610@gmail.com>
2021-11-04 10:23:13 +0900

    Add error handle

--- src/public/save-csv.php
+++ src/public/save-csv.php
@@ -1,20 +1,24 @@
 <?php
 
-/** @var PDO $pdo */
-$pdo = require __DIR__.'/../pdo.php';
-$sql = 'SELECT * FROM test_tbl';
-$sth = $pdo->query($sql);
-$data = $sth->fetchAll(PDO::FETCH_ASSOC);
+try {
+    /** @var PDO $pdo */
+    $pdo = require __DIR__ . '/../pdo.php';
+    $sql = 'SELECT * FROM test_tbl';
+    $sth = $pdo->query($sql);
+    $data = $sth->fetchAll(PDO::FETCH_ASSOC);
 
-header('Content-Type: text/csv; charset=UTF-8');
-header('Content-Disposition: attachment; filename=cemi.csv');
-header('Content-Transfer-Encoding: binary');
+    header('Content-Type: text/csv; charset=UTF-8');
+    header('Content-Disposition: attachment; filename=cemi.csv');
+    header('Content-Transfer-Encoding: binary');
 
 
-$fp = fopen('php://output', 'w');
-foreach ($data as $i => $row) {
-    fputcsv($fp, $row, ",", '"');
+    $fp = fopen('php://output', 'w');
+    foreach ($data as $i => $row) {
+        fputcsv($fp, $row, ",", '"');
+    }
+    fclose($fp);
+
+    exit;
+} catch (PDOException $e) {
+    print($e->getMessage());
 }
-fclose($fp);
-
-exit;
