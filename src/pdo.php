167244a22e4411bb505e18065136498926a4f8b7
irnak4t <ima.tak0610@gmail.com>
2021-11-06 01:29:15 +0900

    Set pdo options

--- src/pdo.php
+++ src/pdo.php
@@ -1,4 +1,10 @@
 <?php
 
-return new PDO('mysql:host=db;dbname=test_db;charset=utf8mb4;port=3306', 'root', 'password');
+$opt = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::MYSQL_ATTR_MULTI_STATEMENTS => false,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+
+return new PDO('mysql:host=db;dbname=test_db;charset=utf8mb4;port=3306', 'root', 'password', $opt);
 
